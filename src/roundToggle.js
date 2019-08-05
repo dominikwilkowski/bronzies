0bb6dd5ae61a7beeb26e421fdd8a5c0f3cd9c21f
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-08-05 21:24:51 +1000

    WIP: added toggle animation
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/roundToggle.js
+++ src/roundToggle.js
@@ -1,26 +1,40 @@
 /** @jsx jsx */
+import { animated, useSpring } from 'react-spring';
 import { useGameData } from './app';
 import { jsx } from '@emotion/core';
 import { colors } from './theme';
 import { useState } from 'react';
 
 function RoundToggle() {
-	const {
-		setQuestionsDB,
-	} = useGameData();
+	const { setQuestionsDB } = useGameData();
 	const [ round, setRound ] = useState('Signals');
+	const [ isOpen, setIsOpen ] = useState( false );
+	const { opacity } = useSpring({ opacity: isOpen ? 1 : 0 });
 
 	return (
-		<button type='button' css={{
-			apperance: 'none',
-			border: 'none',
-			background: 'transparent',
-			padding: '1rem',
-			fontSize: '1rem',
-			color: colors[ 0 ],
-			textDecoration: 'underline',
-			cursor: 'pointer',
-		}}>Round: { round }</button>
+		<div css={{
+			position: 'relative',
+		}}>
+			<button type='button' onClick={ () => setIsOpen( !isOpen ) } css={{
+				apperance: 'none',
+				border: 'none',
+				background: 'transparent',
+				padding: '1rem',
+				fontSize: '1rem',
+				color: colors[ 0 ],
+				textDecoration: 'underline',
+				cursor: 'pointer',
+			}}>Round: { round }{isOpen?'open':'closed'}</button>
+			<animated.div style={{ opacity }} css={{
+				position: 'absolute',
+				left: 0,
+				bottom: 0,
+				padding: '2rem',
+				overflow: 'hidden',
+			}}>
+				Pekaboo
+			</animated.div>
+		</div>
 	);
 };
 
