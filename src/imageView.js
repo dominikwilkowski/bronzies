ac0fadf27e4547a49a71edb9281482a8a316567c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-02-04 07:03:55 +0100

    started updateing to the new signals

--- src/imageView.js
+++ src/imageView.js
@@ -1,24 +1,32 @@
 /** @jsxImportSource @emotion/react */
 
-import { jsx } from '@emotion/react';
-import PropTypes from 'prop-types';
-import SVG from './svg';
+import { jsx } from "@emotion/react";
+import PropTypes from "prop-types";
+import SVG from "./svg";
 
 function ImageView({ image, alt }) {
 	return (
-		<figure data-id={ image } css={{
-			textAlign: 'center',
-			margin: 0,
-		}}>
-			<SVG styling={{
-				display: 'block',
-				width: '300px',
-				height: '190px',
-				margin: '0 auto',
-			}} src={ image } title='What is this signal?' description={ alt } />
+		<figure
+			data-id={image}
+			css={{
+				textAlign: "center",
+				margin: 0,
+			}}
+		>
+			<SVG
+				styling={{
+					display: "block",
+					width: "340px",
+					height: "300px",
+					margin: "0 auto",
+				}}
+				src={image}
+				title="What is this signal?"
+				description={alt}
+			/>
 		</figure>
 	);
-};
+}
 
 ImageView.propTypes = {
 	image: PropTypes.string.isRequired,
